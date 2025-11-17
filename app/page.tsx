fb23982bf453eab9e68560aaf23d797deeb319e9
Luca Meusburger <meusburger.luca@gmail.com>
2025-11-17 11:54:23 +0100

    always presentation

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { collectionName, getTexts } from '@/lib/directus';
 
 import FixedButton from '@/components/FixedButton';
 import RealtimeTexts from '@/components/RealtimeTexts';
-import TextItem from '@/components/TextItem';
 
 export default async function Home({
   searchParams,
@@ -16,15 +15,7 @@ export default async function Home({
   return (
     <div className={isPresentation ? "h-screen w-screen overflow-hidden" : "min-h-screen text-5xl"}>
       <main className={isPresentation ? "h-full w-full" : "mx-auto p-4"}>
-        {isPresentation ? (
-          <RealtimeTexts initialTexts={data} collectionName={collectionName} />
-        ) : (
-          <div>
-            {data.map((item) => (
-              <TextItem key={item.id} item={item} />
-            ))}
-          </div>
-        )}
+        <RealtimeTexts initialTexts={data} collectionName={collectionName} />
       </main>
 
       {!isPresentation && (
